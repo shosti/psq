afc618f4a1317be7db68d97a298b857c7d587081
Emanuel Evans <mail@emanuel.industries>
2016-03-21 20:23:37 -0700

    Implement Enumerable/Collectable and test

--- lib/psq.ex
+++ lib/psq.ex
@@ -1,29 +1,66 @@
 defmodule PSQ do
   alias PSQ.Entry
 
-  def empty do
-    []
+  defstruct xs: []
+
+  def new do
+    %PSQ{}
   end
 
-  def insert(q, entry) do
-    [entry | q]
+  def new(xs) do
+    Enum.into xs, new
   end
 
-  def pop([]) do
-    {:empty, []}
+  def to_list(q) do
+    Enum.into q, []
   end
 
-  def pop(q) do
-    min_entry = q |> Enum.min_by(&Entry.priority/1)
+  def insert(q = %PSQ{xs: xs}, entry) do
+    if lookup(q, Entry.key(entry)) do
+      q
+    else
+      %PSQ{xs: [entry | xs]}
+    end
+  end
+
+  def pop(q = %PSQ{xs: []}) do
+    {:empty, q}
+  end
+
+  def pop(q = %PSQ{xs: xs}) do
+    min_entry = xs |> Enum.min_by(&Entry.priority/1)
 
     {{:entry, min_entry}, delete(q, Entry.key(min_entry))}
   end
 
-  def lookup(q, k) do
-    q |> Enum.find(&(Entry.key(&1) == k))
+  def lookup(%PSQ{xs: xs}, k) do
+    xs |> Enum.find(&(Entry.key(&1) == k))
   end
 
-  def delete(q, k) do
-    q |> Enum.reject(&(Entry.key(&1) == k))
+  def delete(%PSQ{xs: xs}, k) do
+    %PSQ{xs: xs |> Enum.reject(&(Entry.key(&1) == k))}
+  end
+end
+
+defimpl Enumerable, for: PSQ do
+  def count(%PSQ{xs: xs}), do: count(xs)
+  def member?(%PSQ{xs: xs}, element), do: member?(xs, element)
+
+  def reduce(_,   {:halt, acc}, _fun),           do: {:halted, acc}
+  def reduce(q,   {:suspend, acc}, fun),         do: {:suspended, acc, &reduce(q, &1, fun)}
+  def reduce(%PSQ {xs: []}, {:cont, acc}, _fun), do: {:done, acc}
+  def reduce(q, {:cont, acc}, fun) do
+    {{:entry, x}, rest} = PSQ.pop(q)
+    reduce(rest, fun.(x, acc), fun)
+  end
+end
+
+defimpl Collectable, for: PSQ do
+  def into(original) do
+    {original, fn
+      q, {:cont, x} -> PSQ.insert(q, x)
+      q, :done -> q
+      _, :halt -> :ok
+    end}
   end
 end
--- test/psq_test.exs
+++ test/psq_test.exs
@@ -6,20 +6,59 @@ defmodule PSQTest do
   doctest PSQ
 
   defimpl PSQ.Entry, for: Integer do
-    def priority(x), do: x
+    def priority(x), do: -x
     def key(x), do: x
   end
 
   test "sanity test" do
-    q = empty
-
+    q = new
     q = q |> insert(3) |> insert(2) |> insert(5) |> insert(1) |> insert(4)
 
-    {{:entry, 1}, q} = q |> pop
-    {{:entry, 2}, q} = q |> pop
-    {{:entry, 3}, q} = q |> pop
-    {{:entry, 4}, q} = q |> pop
     {{:entry, 5}, q} = q |> pop
+    {{:entry, 4}, q} = q |> pop
+    {{:entry, 3}, q} = q |> pop
+    {{:entry, 2}, q} = q |> pop
+    {{:entry, 1}, q} = q |> pop
     {:empty, _} = q |> pop
+  end
+
+  property :sort do
+    for_all xs in list(int) do
+      q = new(xs)
+      l = to_list(q)
+
+      l == Enum.reverse(Enum.sort(Enum.uniq(xs)))
+    end
+  end
+
+  property :minimum do
+    for_all xs in list(int) do
+      q = new(xs)
+      case pop(q) do
+        {{:entry, min}, _} ->
+          Enum.all? xs, fn(x) ->
+            x <= min
+          end
+        {:empty, _} -> true
+      end
+    end
+  end
+
+  property :membership do
+    for_all xs in list(int) do
+      q = new(xs)
+      Enum.all? xs, fn(x) ->
+        lookup(q, x) != nil
+      end
+    end
+  end
+
+  property :deletion do
+    for_all xs in list(int) do
+      q = new(xs)
+      Enum.all? xs, fn(x) ->
+        lookup(delete(q, x), x) == nil
+      end
+    end
   end
 end
