14178c797e66eb9001366020efec56b82b1acc2b
Emanuel Evans <mail@emanuel.industries>
2016-07-09 12:11:22 -0700

    Relax elixir version requirements

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PSQ.Mixfile do
   def project do
     [app: :psq,
      version: "0.0.1",
-     elixir: "~> 1.3",
+     elixir: ">= 1.2.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
