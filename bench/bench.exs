d9d6766e95537555588bec2c176b7f5586736ada
Emanuel Evans <mail@emanuel.industries>
2016-07-10 19:49:02 -0700

    Tweaks for benchmarking
    - Update for new API
    
    - Add 10_000_000 case (feasible, so why not...)
    
    - Don't include rev in CSV (might as well check in results at the same
      time)

--- bench/bench.exs
+++ bench/bench.exs
@@ -4,19 +4,19 @@ defmodule PSQBench do
     PSQ.from_list(list)
   end
 
-  def insert(q, n) do
+  def put(q, n) do
     # We want a probably-new element in the right range, so we square first
     x = :rand.uniform(n * n)
-    PSQ.insert(q, x)
+    PSQ.put(q, x)
   end
 
   def pop(q) do
     PSQ.pop(q)
   end
 
-  def lookup(q, n) do
+  def get(q, n) do
     x = :rand.uniform(n)
-    PSQ.lookup(q, x * x)
+    PSQ.get(q, x * x)
   end
 
   def delete(q, n) do
@@ -25,18 +25,19 @@ defmodule PSQBench do
   end
 end
 
-{out, 0} = System.cmd "git", ~w(rev-parse HEAD)
-rev = String.strip(out) |> String.slice(0..6)
 date = DateTime.utc_now |> DateTime.to_unix
-fname = Path.join([System.cwd, "bench", "data", "#{date}-#{rev}.csv"])
+fname = Path.join([System.cwd, "bench", "data", "#{date}.csv"])
 file = File.open! fname, [:write]
-b = Enum.reduce [10_000, 100_000, 1_000_000], Benchee.init, fn (n, b) ->
+
+b = Enum.reduce [10_000, 100_000, 1_000_000, 10_000_000], Benchee.init, fn (n, b) ->
+  IO.puts("Making PSQ of size #{n}...")
   q = PSQBench.q(n)
+  IO.puts("Made PSQ!")
 
   b
-  |> Benchee.benchmark("insert #{n}", fn -> PSQBench.insert(q, n) end)
+  |> Benchee.benchmark("put #{n}", fn -> PSQBench.put(q, n) end)
   |> Benchee.benchmark("pop #{n}", fn -> PSQBench.pop(q) end)
-  |> Benchee.benchmark("lookup #{n}", fn -> PSQBench.lookup(q, n) end)
+  |> Benchee.benchmark("get #{n}", fn -> PSQBench.get(q, n) end)
   |> Benchee.benchmark("delete #{n}", fn -> PSQBench.delete(q, n) end)
 end
 
